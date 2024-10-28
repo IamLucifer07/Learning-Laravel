72b26c1a67b953ef0cee08308b3b3fbdc7f9f8a2
Gaurav Chaulagain <talktogaurav07@gmail.com>
2024-10-28 09:09:23 +0545

    can

--- example/resources/views/jobs/show.blade.php
+++ example/resources/views/jobs/show.blade.php
@@ -6,7 +6,10 @@
     <h2 class="font-bold text-lg">{{ $job->title }}</h2>
     <p>This job pays {{ $job->salary }} per year.</p>
 
+
+    @can('edit-job', $job)
     <p class="mt-6">
         <x-button href="/jobs/{{ $job->id }}/edit">Edit</x-button>
     </p>
+    @endcan
 </x-layout>
