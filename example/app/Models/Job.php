3b59bdbc34716c7a184cfdc25ec38364617f2658
Gaurav Chaulagain <talktogaurav07@gmail.com>
2024-12-08 17:34:31 +0545

    job

--- example/app/Models/Job.php
+++ example/app/Models/Job.php
@@ -13,10 +13,7 @@ class Job extends Model
 
     protected $fillable = ['title', 'salary', 'employer_id'];
 
-    public function employer()
-    {
-        return $this->belongsTo(Employer::class);
-    }
+
 
     public function tags()
     {
