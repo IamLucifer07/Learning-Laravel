b5e76091651127173aa7c2901f4f96a02e953e8b
Gaurav Chaulagain <talktogaurav07@gmail.com>
2025-01-02 20:02:19 +0545

    nothing

--- example/resources/views/jobs/edit.blade.php
+++ example/resources/views/jobs/edit.blade.php
@@ -42,7 +42,7 @@
             <div class="flex items-center">
                 <button form="delete-form" type="submit" class="rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600">Delete</button>
             </div>
-
+//
             <div class="flex items-center gap-x-6">
                 <a href="/jobs/{{ $job->id }}" class="text-sm font-semibold leading-6 text-gray-900">Cancel</a>
                 <div>
